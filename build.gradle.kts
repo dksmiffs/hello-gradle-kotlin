23a6c3becccda2a3f91137a7b2e6ef7e392caf4e
Dave Smith <dave.k.smith@gmail.com>
2018-09-17 18:58:00 -0500

    update to Kotlin 1.2.70, Gradle 4.10.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@
 //    gradle/kotlin-dsl/blob/master/samples/hello-kotlin/build.gradle.kts
 plugins {
   application
-  kotlin("jvm") version "1.2.61"
+  kotlin("jvm") version "1.2.70"
 }
 
 application {
